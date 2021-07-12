9321ca732864ef4a52d7b82e4a3163904e0d92aa
Emerson Bezerra <emersondevelops@gmail.com>
2021-07-12 11:31:39 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/zup/academy/FretesGrpcServer.kt
@@ -0,0 +1,28 @@
+package br.com.zup.academy
+
+import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class FretesGrpcServer : GrpcFretesServiceGrpc.GrpcFretesServiceImplBase() {
+
+    private val logger = LoggerFactory.getLogger(FretesGrpcServer::class.java)
+
+    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+
+        logger.info("Calculando frete para request: $request")
+
+        val response = CalculaFreteResponse.newBuilder()
+            .setCep(request!!.cep)
+            .setValor(Random.nextDouble(from = 10.0, until = 140.0))
+            .build()
+
+        logger.info("Frete calculado: $response")
+
+        responseObserver!!.onNext(response)
+        responseObserver.onCompleted()
+
+    }
+}
