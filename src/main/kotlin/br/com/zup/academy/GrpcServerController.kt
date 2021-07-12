7e0879d8adda97d7a46269736b90f512bf522dcf
Emerson Bezerra <emersondevelops@gmail.com>
2021-07-12 16:16:49 -0300

    Habilita monitoramento da aplicação sobre HTTP

---
+++ src/main/kotlin/br/com/zup/academy/GrpcServerController.kt
@@ -0,0 +1,17 @@
+package br.com.zup.academy
+
+import io.micronaut.grpc.server.GrpcEmbeddedServer
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import javax.inject.Inject
+
+@Controller
+class GrpcServerController(@Inject val grpcServer: GrpcEmbeddedServer) {
+
+    @Get("/stop")
+    fun stop(): HttpResponse<String> {
+        grpcServer.stop()
+        return HttpResponse.ok("is running? ${grpcServer.isRunning}")
+    }
+}
